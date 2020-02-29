de1d2bb7f4522b5a82fdbd06acc82539835f2c3e
Marcos Huck <marcos.h3012@gmail.com>
2020-02-29 16:49:57 -0300

    Add user entity and dto

--- users/user_entity.go
+++ users/user_entity.go
@@ -1,7 +1,26 @@
 package users
 
-import "github.com/seuc-frp-utn/api/database"
+import (
+	"github.com/seuc-frp-utn/api/database"
+	"time"
+)
 
 type User struct {
 	database.Model
+	FirstName string `json:"first_name"`
+	MiddleName *string `json:"middle_name,omitempty"`
+	LastName string `json:"last_name"`
+	Email string `json:"email" gorm:"unique"`
+	Birthday time.Time `json:"birthday"`
+	Password string `json:"-"`
+	Salt string `json:"-"`
+}
+
+type UserCreate struct {
+	FirstName string `json:"first_name"`
+	MiddleName *string `json:"middle_name,omitempty"`
+	LastName string `json:"last_name"`
+	Email string `json:"email"`
+	Birthday time.Time `json:"birthday"`
+	Password string `json:"password"`
 }
