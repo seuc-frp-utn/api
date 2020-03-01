a7000a1ed083579b0889b94ac2bb139f60e17593
Marcos Huck <marcos.h3012@gmail.com>
2020-03-01 01:53:05 -0300

    Fix imports on users.go

--- users/users.go
+++ users/users.go
@@ -22,13 +22,13 @@ func initialize() {
 
 func Register(group *gin.RouterGroup) *gin.RouterGroup {
 	initialize()
-	group.Use(middlewares.Handler)
+	group.Use(middlewares.JWT)
 	{
-		group.GET("/:uuid", middlewares.Handler(roles.USER|roles.OPERATOR|roles.ADMIN), (*UserController).Read)
-		group.GET("/", middlewares.Handler(roles.OPERATOR|roles.ADMIN), (*UserController).ReadAll)
-		group.POST("/", middlewares.Handler(roles.OPERATOR|roles.ADMIN), (*UserController).Create)
-		group.PUT("/:uuid", middlewares.Handler(roles.OPERATOR|roles.ADMIN), (*UserController).Update)
-		group.DELETE("/:uuid", middlewares.Handler(roles.OPERATOR|roles.ADMIN), (*UserController).Remove)
+		group.GET("/:uuid", middlewares.Roles(roles.USER|roles.OPERATOR|roles.ADMIN), (*UserController).Read)
+		group.GET("/", middlewares.Roles(roles.OPERATOR|roles.ADMIN), (*UserController).ReadAll)
+		group.POST("/", middlewares.Roles(roles.OPERATOR|roles.ADMIN), (*UserController).Create)
+		group.PUT("/:uuid", middlewares.Roles(roles.OPERATOR|roles.ADMIN), (*UserController).Update)
+		group.DELETE("/:uuid", middlewares.Roles(roles.OPERATOR|roles.ADMIN), (*UserController).Remove)
 	}
 	return group
 }
